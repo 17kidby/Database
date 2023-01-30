eaed48abecd673ab0ca920fe1537fe6a1b27b8ec
17kidby_m <17kidby_m@bexleygs.co.uk>
2023-01-30 11:19:09 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test the database works
+        System.out.println("Database");
+        Database db = new Database("test.txt", 10);
+        db.appendRecord("1.ABCDEFGH");
+        db.appendRecord("2.ABCDEFGH");
+        db.appendRecord("3.ABCDEFGH");
+        db.appendRecord("4.ABCDEFGH");
+        System.out.println(db.getRecord(2));	 // should be "3.ABCDEFGH"
+        System.out.println(db.findRecord("EBCDEFGHIJ")); // false
+        System.out.println(db.findRecord("2.ABCDEFGH")); // true
+        System.out.println("count = " + db.getRecordCount()); // 4
+        FileHandler.writeLineAt("test.txt", "sans", 0);
+    }
+}
